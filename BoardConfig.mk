98a28d39aaf270b2dc467ddcf3c0626ef46fba9b
thepriyanshujangid <priyanshujangid@yahoo.com>
2024-05-09 21:27:25 +0530

    taoyao:  Switch to `Prebuilt Kernel`

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,17 +12,6 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := taoyao
 
-# Kernel
-TARGET_KERNEL_CONFIG += vendor/taoyao_QGKI.config
-
-# Kernel modules
-BOOT_KERNEL_MODULES := \
-    goodix_core.ko \
-    hwid.ko \
-    msm_drm.ko \
-    xiaomi_touch.ko
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
-
 # Partitions
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 113254576128
